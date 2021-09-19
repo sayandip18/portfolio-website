d0c6aca1588b7f3c9c165d6e93e245fed967e9a8
sayandiph <sayandiph4@gmail.com>
2021-09-19 14:42:13 +0530

    add geometry

--- main.js
+++ main.js
@@ -17,3 +17,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+
+// Adding geometry
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
+
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus)
