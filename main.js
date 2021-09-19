48d527fb2807494e403d68bcbaccc6bab73c973b
sayandiph <sayandiph4@gmail.com>
2021-09-19 14:43:16 +0530

    add stars

--- main.js
+++ main.js
@@ -39,3 +39,17 @@ scene.add(lightHelper, gridHelper);
 
 // Adding orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
+
+// Adding 3d stars and positioning them randomly
+function addStar(){
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );
+
+    star.position.set(x,y,z);
+    scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
