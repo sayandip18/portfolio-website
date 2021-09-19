ce5e0744595e487a409ef2dd6aaf2ecddb1f012c
sayandiph <sayandiph4@gmail.com>
2021-09-19 14:41:39 +0530

    add scene, camera, renderer

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+// Camera and scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
